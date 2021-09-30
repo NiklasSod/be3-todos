25f799f89871cef0bb0324db15bd433c36fa310d
Georgios <test@example.com>
2021-09-30 14:59:25 +0200

    mongose connection

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const userRoute = require("./routes/usersRoute");
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/be3-todo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+
 const port = process.env.PORT || 3001;
 
 app.use("/users/", userRoute);
