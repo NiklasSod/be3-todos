a06a3151b90a2c4d7bc8d9b9687a8670b22f20c9
Georgios <test@example.com>
2021-10-05 15:00:21 +0200

    passwor crypt

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const User = require("./../models/users");
+const bcrypt = require('bcrypt');
 
 exports.showRegisterPage = (req, res) => {
   res.json({ message: "Hello from registerpage!" });
@@ -9,32 +10,32 @@ exports.registerUser = (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
 
+
+  
+  
   const newUser = new User({
     name,
     email,
     password,
   });
 
-  newUser.save();
-  return res.status(200).json({ msg: newUser });
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) console.log(err);
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      //Save pass to hash
+      newUser.password = hash;
+      //Save user
+      newUser
+        .save()
+        .then((value) => {
+          console.log('The following user was created:');
+          console.log(value);
+          const message = 'You have successfuly registered!';
+          res.render('login', { message });
+        })
+        .catch((value) => console.log(value));
+    });
+  });
 };
 
-// app.get('/createuser', function(req, res){
-
-//   var user = new User();
-
-//   user.name = req.body.name;
-//   user.city = req.body.city;
-
-//   if(req.body.type == "typeA"){
-//       user.accounts.typeA.isUser = true;
-//   }
-//   if(type == "typeB"){
-//       user.accounts.typeB.isUser = true;
-//   }
-
-//   user.save(function(err, user){
-//       if(err) return err;
-//       res.send(user);
-//   });
-// });
